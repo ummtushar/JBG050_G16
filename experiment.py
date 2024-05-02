bb81ead60c8883436c60a54b945fd97d99cbddb2
Tushar Gupta <114675969+ummtushar@users.noreply.github.com>
2024-05-02 19:59:44 +0200

    linear regression for every borough based on PAS and arrest data

---
+++ experiment.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+from minisom import MiniSom
+from sklearn.preprocessing import MinMaxScaler
+import folium
+from textblob import TextBlob
+import geopandas as gpd
+from matplotlib.colors import Normalize, rgb2hex
+from branca.colormap import LinearColormap
+
+df = pd.read_csv('data/Borough-Table 1.csv', delimiter = ';')  
+df = df[['Date', 'Survey', 'Borough', 'Measure', 'Proportion', 'MPS']]
+
+#LDA
+# texts = df['Measure'].tolist()
+# vectorizer = CountVectorizer(stop_words='english')
+# doc_term_matrix = vectorizer.fit_transform(texts)
+
+# lda = LatentDirichletAllocation(n_components=2, random_state=42)
+# lda.fit(doc_term_matrix)
+
+# # Display topics
+# for i, topic in enumerate(lda.components_):
+#     print(f"Top words for topic #{i}:")
+#     print([vectorizer.get_feature_names_out()[index] for index in topic.argsort()[-10:]])
+#     print("\n")
+
+################################################ Mapping the trust on the map ############################################################
+# # Calculate sentiment score for each entry
+# df['Sentiment_Score'] = df['Measure'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+# # Group by borough and count the number of positive feedbacks
+# positive_feedback_count = df[df['Sentiment_Score'] > 0].groupby('Borough').size().reset_index(name='Positive_Feedback_Count')
+
+# # Load London borough boundaries
+# london_boroughs = gpd.read_file("data/london_boroughs.geojson")
+
+# # Merge positive feedback count with borough boundaries and handle missing data
+# london_boroughs_feedback = london_boroughs.merge(positive_feedback_count, left_on='name', right_on='Borough', how='left')
+# london_boroughs_feedback['Positive_Feedback_Count'].fillna(0, inplace=True)
+
+# # Check for CRS information and convert if necessary
+# if london_boroughs_feedback.crs is None:
+#     london_boroughs_feedback.set_crs(epsg=4326, inplace=True)  # Assuming the GeoJSON is in WGS 84
+# london_boroughs_feedback = london_boroughs_feedback.to_crs(epsg=3857)  # Converting to Web Mercator for area calculations
+# london_boroughs_feedback['Borough_Area'] = london_boroughs_feedback['geometry'].area / 1e6  # Convert area from sq meters to sq kilometers
+
+# # Scaling positive feedback counts for color mapping
+# scaler = MinMaxScaler()
+# london_boroughs_feedback['Scaled_Feedback'] = scaler.fit_transform(london_boroughs_feedback[['Positive_Feedback_Count']])
+
+# # Create a colormap for positive feedback
+# feedback_colormap = LinearColormap(colors=['#ffcccc', '#990000'], vmin=0, vmax=1, caption='Positive Feedback Intensity')
+
+# # Create a map of London
+# london_map = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
+
+# # Function to adjust circle size
+# def radius_picker(count):
+#     return 5 + 20 * count  # Adjust this formula to control the radius size
+
+# # Add GeoJson to map to show boundaries
+# folium.GeoJson(london_boroughs_feedback, name='London Boroughs').add_to(london_map)
+
+# # Add boroughs to the map with circle size and color intensity based on feedback
+# for index, row in london_boroughs_feedback.iterrows():
+#     feedback_color = rgb2hex(feedback_colormap(row['Scaled_Feedback']))
+#     folium.CircleMarker(
+#         location=[row.geometry.centroid.y, row.geometry.centroid.x],
+#         radius=radius_picker(row['Positive_Feedback_Count']),
+#         color='grey',
+#         fill=True,
+#         fill_color=feedback_color,
+#         fill_opacity=0.7,
+#         popup=f"Borough: {row['name']}<br>Positive Feedback: {row['Positive_Feedback_Count']}"
+#     ).add_to(london_map)
+
+# feedback_colormap.add_to(london_map)
+# london_map.save('london_sentiment_map.html')
+
+########################################  ####################################################
+
---
+++ reg.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+import torch
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+df1 = pd.read_csv('data/Borough-Table 1.csv', delimiter=';', decimal=",")
+df2 = pd.read_csv('data/MPS Custody - Arrests - 2019 01 to 2024 03.csv', delimiter=';')
+
+df1['Date'] = pd.to_datetime(df1['Date'], format='%m/%d/%y').dt.year
+# df1['Date'] = pd.to_datetime(df1['Date'])
+df2 = df2.rename(columns= {'Arrest Year': 'Date'})
+
+df2['Date'] = pd.to_datetime(df2['Date']).astype(str).str.extract(r'(\d{4})$')
+df2['Date'] = pd.to_datetime(df2['Date']).dt.year
+
+#processing the PAS dataset
+df1 = df1.drop(columns = ['Survey', 'MPS', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9'])
+# df1 = df1.groupby(by = ['Date', 'Borough', 'Measure']).mean()
+# df1 = df1.loc[2019:,:]
+# print(df1.head())
+# print(df1.dtypes)
+
+#processing the arrest dataset
+df2 = df2.drop(columns = ['Arrest Month', 'Arrest Month Name', 'Gender', 'Age Group', 'Domestic Abuse Flag', 'First Arrest Offnece'])
+df2 = df2.rename(columns={'Ethnicity (4+1)': 'Ethnicity'})
+# df2 = df2.groupby(by=["Date", 'Ethnicity (4+1)']).mean()
+# print(df2.head())
+# print(df2.dtypes)
+
+
+#merged dataset
+df_merged = pd.merge(df1, df2, on=['Date'], how='outer')
+df_merged = df_merged.dropna()
+# print(df_merged.head())
+
+# linear regression
+boroughs = df_merged['Borough'].unique()
+
+# One-Hot Encoding for 'Measure' column
+df_merged = pd.get_dummies(df_merged, columns=['Measure'])
+
+# Label Encoding for 'Ethnicity' column
+le = LabelEncoder()
+df_merged['Ethnicity'] = le.fit_transform(df_merged['Ethnicity'])
+ethnicity_mapping = {index: label for index, label in enumerate(le.classes_)}
+print(ethnicity_mapping)
+
+for borough in boroughs:
+    df_borough = df_merged[df_merged['Borough'] == borough]
+    
+    X = df_borough.drop(['Proportion', 'Borough'], axis=1)
+    y = df_borough['Proportion']
+
+    # Normalize features on the X
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    # print(f"Score for {borough}: {model.score(X_test, y_test)}")
+
+    #coeffcients
+    print(f"Coefficients for {borough}: {model.coef_}")
+    
+    #MSE
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"MSE for {borough}: {mse}\n")
