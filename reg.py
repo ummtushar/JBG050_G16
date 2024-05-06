64271d704491e947a11834f40b357f9930da6b40
Tushar Gupta <114675969+ummtushar@users.noreply.github.com>
2024-05-06 18:53:47 +0200

    added plots

--- reg.py
+++ reg.py
@@ -5,6 +5,7 @@ import torch
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 ############################ LOADING THE DATA SETS ############################
 
@@ -38,6 +39,8 @@ def trust_factors(df_merged, one_hot):
     '''
     boroughs = df_merged['Borough'].unique()
 
+    borough_coefs = {}
+
     # One-Hot Encoding for 'Measure' column
     for i in one_hot:
         df_merged = pd.get_dummies(df_merged, columns=[i])
@@ -59,12 +62,32 @@ def trust_factors(df_merged, one_hot):
         model.fit(X_train, y_train)
 
         #coeffcients
+        borough_coefs[borough] = dict(zip(columns, model.coef_))
         print(f"Coefficients for {borough}: {dict(zip(columns, model.coef_))}") 
 
         #MSE
         y_pred = model.predict(X_test)
         mse = mean_squared_error(y_test, y_pred)
         print(f"MSE for {borough}: {mse}\n")
+
+    # Plot coefficients for each borough
+    plt.figure(figsize=(15, 10))
+
+    fig, ax = plt.subplots()
+
+    for borough, coefs in borough_coefs.items():
+        ax.bar(coefs.keys(), coefs.values(), label=borough)
+
+    ax.set_xlabel('Coefficient Name')
+    ax.set_ylabel('Coefficient Value')
+
+    # Set legend size
+    ax.legend(prop={'size': 6})
+
+    # Rotate x-axis labels
+    plt.xticks(rotation=45)
+
+    plt.show()
 
 
 trust_factors(df_merged, ['Measure', 'Ethnicity'])
