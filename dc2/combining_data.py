408792a3575376d293aa387786321cc641773a1c
Dani Vuijk <d.vuijk@student.tue.nl>
2024-06-05 16:09:06 +0200

    final push

--- dc2/combining_data.py
+++ dc2/combining_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from pathlib import Path
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 EDA = False # set to True to show the head of each dataset
 data_path = Path('data/Final datasets cleaned')
-yearly_data = ['custody_arrests_cleaned.csv', 'employment_cleaned.csv', 'ethnic_groups_cleaned.csv']
+yearly_data = ['employment_cleaned.csv', 'ethnic_groups_cleaned.csv']
 
 if EDA:
 # print the head of each dataset
@@ -18,7 +19,6 @@ if EDA:
 # Get the start and end date of the datasets
 start_dates = {}
 end_dates = {}
-all_boroughs = set()
 for file in data_path.iterdir():
     if file.suffix != '.csv':
         continue
@@ -26,8 +26,6 @@ for file in data_path.iterdir():
     print(file.name) 
     start_dates[file.name] = df['Date'].min()
     end_dates[file.name] = df['Date'].max()
-    if 'Borough' in df.columns:
-        all_boroughs.update(df['Borough'].unique())
 
 min_date = min(start_dates.values())
 max_date = max(end_dates.values())
@@ -44,7 +42,33 @@ for file, end_date in end_dates.items():
 
 pas_path = data_path / 'pas_cleaned.csv'
 main_df = pd.read_csv(pas_path)
+mistyped_boroughs = {
+        'Barking And Dagenham': 'Barking and Dagenham',
+        'City of Westminster': 'Westminster',
+        'Hammersmith And Fulham': 'Hammersmith and Fulham',
+        'Kensington And Chelsea': 'Kensington and Chelsea',
+        'Kingston upon Thames': 'Kingston',
+        'Kingston Upon Thames': 'Kingston',
+        'Richmond upon Thames': 'Richmond',
+        'Richmond Upon Thames': 'Richmond',
+        'Hammersmith & Fulham': 'Hammersmith and Fulham',
+        'Kensington & Chelsea': 'Kensington and Chelsea',
+        'Barking & Dagenham': 'Barking and Dagenham',
+}
+
+
+main_df['Borough'] = main_df['Borough'].replace(mistyped_boroughs)
+all_boroughs = main_df['Borough'].unique()
 print(main_df.head())
+
+custody_arrest_df = pd.read_csv(data_path / 'custody_arrests_cleaned.csv')
+if 'Borough' not in custody_arrest_df.columns:
+    boroughs_df = pd.DataFrame({'Borough': list(all_boroughs)})
+    custody_arrest_df = custody_arrest_df.merge(boroughs_df, how='cross')
+    # save the new DataFrame
+    custody_arrest_df.to_csv(data_path / 'custody_arrests_cleaned.csv', index=False)
+
+
 for file in data_path.iterdir():
     if file.suffix != '.csv':
         continue
@@ -52,30 +76,31 @@ for file in data_path.iterdir():
         continue
     if file.name == 'pas_cleaned.csv':
         continue
+    if file.name == 'combined_data.csv':
+        continue
     df = pd.read_csv(file)
+    df['Borough'] = df['Borough'].replace(mistyped_boroughs)
     #add the name of the file to the column names
     df.columns = [f'{file.stem}_{col}' if col not in ['Date', 'Borough'] else col for col in df.columns]
 
-    # Create a MultiIndex with all combinations of dates and boroughs
-    df = df.groupby(['Date', 'Borough']).sum().reset_index()
-    multi_index = pd.MultiIndex.from_product([all_dates, all_boroughs], names=['Date', 'Borough'])
+    # Group all the dates to the nearest quarter
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.to_period('Q').dt.to_timestamp()
+    df = df.groupby(['Date', 'Borough']).mean().reset_index()
 
-    # Reindex the DataFrame to include all combinations, filling missing values with 0
-    #df = df.set_index(['Date', 'Borough']).reindex(multi_index, fill_value=0).reset_index()
     # Join the data to the main DataFrame
     main_df['Date'] = pd.to_datetime(main_df['Date'])
+    main_df['Date'] = main_df['Date'].dt.to_period('Q').dt.to_timestamp()
     df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.to_period('Q').dt.to_timestamp()
+    print(file.name)
+    print(df)
     main_df = pd.merge(main_df, df, on=['Date', 'Borough'], how='left')
+
 
 
 for dataset in yearly_data:
     df = pd.read_csv(data_path / dataset)
-
-    # If there are no boroughs, duplicate the data for each borough
-    if 'Borough' not in df.columns:
-        boroughs = all_boroughs
-        boroughs_df = pd.DataFrame({'Borough': list(boroughs)})
-        df = df.merge(boroughs_df, how='cross')
 
     #add the name of the file to the column names
     df.columns = [f'{dataset}_{col}' if col not in ['Date', 'Borough'] else col for col in df.columns]
@@ -86,15 +111,62 @@ for dataset in yearly_data:
     df['Date'] = pd.to_datetime(df['Year'].astype(str) + '-' + df['Month'].astype(str))
     df.drop(columns=['Year', 'Month'], inplace=True)
 
-    main_df['Date'] = pd.to_datetime(main_df['Date'])
+    # Group all the dates to the nearest quarter
     df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.to_period('Q').dt.to_timestamp()
+    df = df.groupby(['Date', 'Borough']).mean().reset_index()
+
+    # Join the data to the main DataFrame
+    main_df['Date'] = pd.to_datetime(main_df['Date'])
+    main_df['Date'] = main_df['Date'].dt.to_period('Q').dt.to_timestamp()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.to_period('Q').dt.to_timestamp()
     main_df = pd.merge(main_df, df, on=['Date', 'Borough'], how='left')
 
 print(main_df.head())
 main_df.to_csv(data_path / 'combined_data.csv', index=False)
+
+lagged_columns = [
+    "time_spent_cleaned_Abstraction Type_Aid",
+    "time_spent_cleaned_Abstraction Type_CAD",
+    "time_spent_cleaned_Abstraction Type_Court",
+    "time_spent_cleaned_Abstraction Type_Custody",
+    "time_spent_cleaned_Abstraction Type_Local Aid",
+    "time_spent_cleaned_Abstraction Type_Not Abstracted",
+    "time_spent_cleaned_Abstraction Type_Staffing Up",
+    "time_spent_cleaned_Abstraction Type_Training",
+    "use_of_force_cleaned_CED Used",
+    "use_of_force_cleaned_Firearms Aimed",
+#    "outcomes_cleaned_Outcome type_Formal action is not in the public interest",
+#    "outcomes_cleaned_Outcome type_Investigation complete; no suspect identified",
+#    "outcomes_cleaned_Outcome type_Local resolution",
+#    "outcomes_cleaned_Outcome type_Offender given a caution",
+#    "outcomes_cleaned_Outcome type_Offender given a drugs possession warning",
+#    "outcomes_cleaned_Outcome type_Offender given penalty notice",
+#    "outcomes_cleaned_Outcome type_Suspect charged",
+#    "outcomes_cleaned_Outcome type_Suspect charged as part of another case",
+#    "outcomes_cleaned_Outcome type_Unable to prosecute suspect",
+#    "outcomes_cleaned_Total",
+]
+
+# Add a time lag for the lagged columns
+borough_counts = len(all_boroughs)
+for col in lagged_columns:
+    # sort by date and boroughs
+    main_df = main_df.sort_values(['Date', 'Borough'])
+    main_df[f'{col}_lag'] = main_df[col].shift(borough_counts - 1)
+
 # Final preperation for regression
+# drop duplicates
 # fill NA with column mean
 main_df = pd.get_dummies(main_df, columns=['Borough'], drop_first=True, dtype=int)
+
+# Add a binary column for covid
+covid_start = pd.to_datetime('2020-03-26') # First day of lockdown
+covid_end = pd.to_datetime('2021-07-19') # End of most legal limits
+main_df['Covid'] = 0
+main_df.loc[(main_df['Date'] >= covid_start) & (main_df['Date'] <= covid_end), 'Covid'] = 1
+
 
 # Ensure 'Date' is in datetime format if needed and extract useful time-based features
 main_df['Date'] = pd.to_datetime(main_df['Date'])
@@ -108,11 +180,23 @@ main_df.fillna(main_df.mean(), inplace=True)
 main_df['Date'] = pd.to_datetime(main_df['Date'])
 main_df['Date'] = main_df['Date'].astype(int) / 10**9
 
+
+
 # rename target to Y
-target = 'Proportion Trust MPS_x'
 target = 'Proportion Relied on to be there'
 
 main_df.rename(columns={target: 'Y'}, inplace=True)
+
+# Perform VIF to remove strongly colinear variables
+main_X = main_df.copy()
+vif_data = pd.DataFrame()
+vif_data["feature"] = main_X.columns
+vif_data["VIF"] = [variance_inflation_factor(main_X.values, i) for i in range(len(main_X.columns))]
+print(vif_data)
+vif_data = vif_data[vif_data["VIF"] < 10]
+
+main_df = main_df[vif_data["feature"].values.tolist()]
+
 
 
 # Save the combined DataFrame
--- dc2/regression.r
+++ dc2/regression.r
@@ -1,43 +1,53 @@
 
 # load the data
+#data <- read.csv("data/Final datasets cleaned/no_multicollinearity.csv")
 data <- read.csv("data/Final datasets cleaned/combined_data.csv")
 
+# print the columns
+
 # remove columns from the data to avoid multicollinearity
-columns_to_remove <- c(
-  "stops_cleaned_Age_Group_Unknown",
-  "stops_cleaned_Subject_Person",
-  "stops_cleaned_Reason.for.Stop_A.Stolen.property..s.1.PACE.",
-  "stops_cleaned_Outcome_1.No.further.action",
-  "stops_cleaned_Outcome.Reason_B.Drugs",
-  "stops_cleaned_Gender_F",
-  "stops_cleaned_Ethnic.Appearance.Code_0.Unknown",
-  "stops_cleaned_EA.Group_Other",
-  "stops_cleaned_Self.defined.Ethnicity.Code_O9.Any.other.et",
-  "stops_cleaned_SDE.Group_Other",
-  "time_spent_cleaned_Abstraction.Type_Not.Abstracted",
-  "granular_cleaned_Household_Other",
-  "granular_cleaned_Birth.country_UK",
-  "granular_cleaned_Disability_No",
-  "granular_cleaned_Religion_No.religion",
-  "granular_cleaned_Gender_Female",
-  "granular_cleaned_Age_Refused",
-  "granular_cleaned_Orientation_Refused",
-  "granular_cleaned_Ethnicity_Other",
-  "granular_cleaned_Qualification_Other",
-  "granular_cleaned_Employment_Other",
-  "use_of_force_cleaned_SubjectEthnicity_Other",
-  "custody_arrests_cleaned.csv_Gender_Female",
-  "custody_arrests_cleaned.csv_Age.Group_Not.Recorded",
-  "custody_arrests_cleaned.csv_Ethnicity..4.1._Other",
-  "custody_arrests_cleaned.csv_First.Arrest.Offnece_Other.Offence",
-  "custody_arrests_cleaned.csv_Domestic.Abuse.Flag_No",
-  "ethnic_groups_cleaned.csv_Mixed..Other..",
-  "Borough_Barnet",
-  "Year_2015"
-)
+#columns_to_remove <- c(
+#  "stops_cleaned_Age_Group_Unknown",
+#  "stops_cleaned_Subject_Person",
+#  "stops_cleaned_Reason.for.Stop_A.Stolen.property..s.1.PACE.",
+#  "stops_cleaned_Outcome_1.No.further.action",
+#  "stops_cleaned_Outcome.Reason_B.Drugs",
+#  "stops_cleaned_Gender_F",
+#  "stops_cleaned_Ethnic.Appearance.Code_0.Unknown",
+#  "stops_cleaned_EA.Group_Other",
+#  "stops_cleaned_Self.defined.Ethnicity.Code_O9.Any.other.et",
+#  "stops_cleaned_SDE.Group_Other",
+#  "time_spent_cleaned_Abstraction.Type_Not.Abstracted",
+#  "granular_cleaned_Household_Other",
+#  "granular_cleaned_Birth.country_UK",
+#  "granular_cleaned_Disability_No",
+#  "granular_cleaned_Religion_No.religion",
+#  "granular_cleaned_Gender_Female",
+#  "granular_cleaned_Age_Refused",
+#  "granular_cleaned_Orientation_Refused",
+#  "granular_cleaned_Ethnicity_Other",
+#  "granular_cleaned_Qualification_Other",
+#  "granular_cleaned_Employment_Other",
+#  "use_of_force_cleaned_SubjectEthnicity_Other",
+#  "custody_arrests_cleaned.csv_Gender_Female",
+#  "custody_arrests_cleaned.csv_Age.Group_Not.Recorded",
+#  "custody_arrests_cleaned.csv_Ethnicity..4.1._Other",
+#  "custody_arrests_cleaned.csv_First.Arrest.Offnece_Other.Offence",
+#  "custody_arrests_cleaned.csv_Domestic.Abuse.Flag_No",
+#  "ethnic_groups_cleaned.csv_Mixed..Other..",
+#  "Borough_Barnet",
+#  "Year_2015"
+#)
 
-data <- data[, !(names(data) %in% columns_to_remove)]
+# data <- data[, !(names(data) %in% columns_to_remove)]
 
-# perform a lassoo regression
+# remove the X column
+data <- data[, -1]
+print(names(data))
+
+
+
+# perform a regression
 m1 <- lm(data = data, formula = Y ~ .)
 summary(m1)
+
